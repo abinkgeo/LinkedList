1469c32133760633891fa80e4a5f4ef2138153fa
abinkgeo <abinkgeo@gmail.com>
2025-12-15 15:44:55 +0530

    added:linked list

--- src/linkedList/Main.java
+++ src/linkedList/Main.java
@@ -1,9 +1,35 @@
+
 package linkedList;
 
 public class Main {
     public static void main(String[] args) {
 
+        MyLinkedList<Integer> list=new MyLinkedList<>();
 
-        System.out.println("");
+        list.insertionAtBeginning(10);
+        list.insertionAtEnd(20);
+        list.insertionAtEnd(30);
+        list.insertionAtEnd(40);
+        list.insertionAtEnd(50);
+        list.display();
+        System.out.println();
+        System.out.println("Size= "+list.getSize());
+
+        System.out.println();
+
+        list.deleteFromBeginning();
+        list.display();
+        System.out.println();
+        System.out.println("Size= "+list.getSize());
+
+
+        list.deleteFromEnd();
+        list.display();
+        System.out.println();
+        System.out.println("Size= "+list.getSize());
+
+        System.out.println(list.search(20));
     }
 }
+
+
---
+++ src/linkedList/MyLinkedList.java
@@ -0,0 +1,111 @@
+package linkedList;
+
+
+
+public class MyLinkedList<T> {
+    private Node<T> head;
+
+    int size=0;
+
+
+    public void insertionAtBeginning(T data){
+        Node<T> newNode=new Node<T>(data);
+        newNode.next=head;
+        head=newNode;
+        size++;
+    }
+
+
+    public void insertionAtEnd(T data){
+        Node<T> newNode=new Node<>(data);
+
+        if(head==null){
+            head=newNode;
+            size++;
+            return;
+        }
+
+        Node<T> temp=head;
+        while(temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=newNode;
+        size++;
+    }
+
+
+    public void deleteFromBeginning(){
+        if(head==null){
+            System.out.println("List is empty");
+            return ;
+        }
+        head=head.next;
+        size--;
+    }
+
+    public void deleteFromEnd(){
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+
+        while(head.next==null){
+            head=null;
+            size--;
+            return;
+        }
+
+        Node <T> temp=head;
+        while(temp.next.next!=null){
+            temp=temp.next;
+        }
+        temp.next=null;
+        size--;
+    }
+
+    public  boolean search(T key){
+
+
+        Node<T> temp=head;
+        while(temp!=null){
+            if(temp.data.equals(key)){
+                return  true;
+            }
+            temp=temp.next;
+        }
+        return false;
+    }
+
+
+    public boolean isEmpty(){
+
+        return head==null;
+    }
+
+    public T getFirst(){
+        if(head==null){
+            System.out.println("List is empty");
+        }
+
+        return head.data;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void display(){
+        Node<T> temp=head;
+
+        while(temp!=null){
+            System.out.print(temp.data+"-->");
+            temp=temp.next;
+        }
+        System.out.print("null");
+    }
+
+
+}
+
+
+
